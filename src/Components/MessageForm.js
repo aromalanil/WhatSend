46723d2357313c60e8dca89d4f7642867966aae0
Aromal Anil <aromalanilkannan@gmail.com>
2020-08-22 19:42:14 +0530

    ⚠ Removed Linting Warning

--- src/Components/MessageForm.js
+++ src/Components/MessageForm.js
@@ -18,7 +18,7 @@ function MessageForm() {
     }
 
     let validatePhoneNumber =() => {
-        
+        // eslint-disable-next-line
         if(phoneNumber.match(/^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/)){
             return true;
         }
