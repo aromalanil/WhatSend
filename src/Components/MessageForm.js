6db762dde2951cdcd0aad4f91960f46113c84f25
Aromal Anil <aromalanilkannan@gmail.com>
2020-08-23 13:14:23 +0530

    🎠 Fixed Responsive Issue

--- src/Components/MessageForm.js
+++ src/Components/MessageForm.js
@@ -12,27 +12,27 @@ function MessageForm() {
     let link = `https://api.whatsapp.com/send?phone=${phoneNumber}${message && `&text=${urlencode(message)}`}`;
 
     let handleLinkClick = () => {
-        if(validatePhoneNumber() && validateMessage()){
+        if (validatePhoneNumber() && validateMessage()) {
             window.location.assign(link);
         }
     }
 
-    let validatePhoneNumber =() => {
+    let validatePhoneNumber = () => {
         // eslint-disable-next-line
-        if(phoneNumber.match(/^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/)){
+        if (phoneNumber.match(/^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/)) {
             return true;
         }
-        else{
+        else {
             setError("Invalid Phone Number");
             return false;
         }
     }
 
-    let validateMessage=()=>{
-        if(message.length<250){
+    let validateMessage = () => {
+        if (message.length < 250) {
             return true;
         }
-        else{
+        else {
             setError("Message can contain only upto 250 characters");
             return false;
         }
@@ -41,24 +41,25 @@ function MessageForm() {
     return (
         <div className="message-form">
             <div className="form-wrapper">
-            <h2>Send Whatsapp Message</h2>
-            <p id="error">{error}</p>
-            <label>Phone Number
+                <h2>Send Whatsapp Message</h2>
+                <p id="error">{error}</p>
+                <label>Phone Number
             <PhoneInput
-                    country={'in'}
-                    value={phoneNumber}
-                    placeholder="+91 987654321"
-                    onChange={phone => setPhoneNumber(phone)}
+                        country={'in'}
+                        value={phoneNumber}
+                        placeholder="+91 987654321"
+                        onChange={phone => setPhoneNumber(phone)}
+                    />
+                </label>
+                <MessageBox
+                    name="Message"
+                    placeholder="Message (Optional)"
+                    value={message}
+                    onChange={message => setMessage(message)}
                 />
-            </label>
-            <MessageBox
-                name="Message"
-                placeholder="Message (Optional)"
-                value={message}
-                onChange={message => setMessage(message)}
-            />
+
+                <button onClick={handleLinkClick} className="message-btn">Send Message</button>
             </div>
-            <button onClick={handleLinkClick} className="message-btn">Send Message</button>
         </div>
     )
 }
