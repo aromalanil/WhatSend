33046d7737cea4b00380257cfccd8458fa94d976
Aromal Anil <aromalanilkannan@gmail.com>
2020-11-05 13:13:27 +0530

    🌑 Added Darkmode support

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import logo from '../img/logo.svg';
 import github_logo from '../img/github_logo.svg';
-import Logo from '../Components/Logo';
-import ImageLink from '../Components/ImageLink';
+import Logo from './Logo';
+import ImageLink from './ImageLink';
+import DarkModeToggle from './DarkModeToggle';
 import { appName, gitRepo } from '../constants';
 
 function NavBar() {
@@ -10,6 +11,7 @@ function NavBar() {
     <nav className="nav-bar">
       <Logo img={logo} title={appName} alt={appName} />
       <div className="nav-links">
+        <DarkModeToggle />
         <ImageLink image={github_logo} link={gitRepo} alt="Github Logo" />
       </div>
     </nav>
---
+++ src/Components/ThemeIcon.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import sunImage from '../img/sun.svg';
+import moonImage from '../img/moon.svg';
+
+function ThemeIcon({ darkMode }) {
+  return <img alt="theme-toggle-icon" className="theme-icon" src={darkMode ? sunImage : moonImage} />;
+}
+
+export default ThemeIcon;
